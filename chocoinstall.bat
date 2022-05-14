0717ad392b2e6f3586a8c875cdb66c74aa012317
H4rl <98224660+H4rldev@users.noreply.github.com>
2022-05-14 21:54:52 +0100

    Create chocoinstall.bat

---
+++ chocoinstall.bat
@@ -0,0 +1,2 @@
+::This installs choco, as found on https://chocolatey.org/docs/installation
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
